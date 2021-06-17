08a0a20f5dcf5bf24c4bf535298780a1f6ad0b3e
John <contact@johnforster.dev>
2021-06-17 12:19:23 +0100

    Remove reference to withTM

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
   reactStrictMode: true,
 }
-
-// Also does not work with withTM:
-// const withTM = require("next-transpile-modules")(["d3-delaunay"]);
-
-// module.exports = withTM();
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Delaunay } from 'd3-delaunay'
 
 export default function Home() {
-  const delaunay = Delaunay.from([[0, 0], [0, 1], [1, 0], [1, 1]])
+  const delaunay = Delaunay.from([[0, 0], [0, 1]])
   console.log(delaunay)
   return (
     <div>
