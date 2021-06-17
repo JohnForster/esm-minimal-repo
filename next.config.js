696de1f6fee0c50fb6606e05579ee9cac8da5521
John <contact@johnforster.dev>
2021-06-17 12:03:59 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// Also does not work with withTM:
+// const withTM = require("next-transpile-modules")(["d3-delaunay"]);
+
+// module.exports = withTM();
+
